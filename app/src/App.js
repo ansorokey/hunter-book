7f6e88714a000c866fbec576262316784e88f46a
ansorokey <ansorokey@gmail.com>
2024-02-22 14:48:46 -0500

    added basic search bar

--- app/src/App.js
+++ app/src/App.js
@@ -1,9 +1,11 @@
+import SearchBar from './components/Search-Bar';
 import './index.css';
 
 function App() {
   return (
     <div id="app-wrapper">
       <h1>Hello World</h1>
+      <SearchBar/>
     </div>
   );
 }
