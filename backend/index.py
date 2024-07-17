210ad9f895fe8432a1aa687cac933e4348fbc27c
ansorokey <ansorokey@gmail.com>
2024-07-17 13:18:50 -0400

    forgot how to import data from other files

--- backend/index.py
+++ backend/index.py
@@ -1,16 +1,29 @@
 import sqlite3
+import monsters from data.py # this is wrong
 
 # Establish the database we want to connect to
 # (Assume we're running this from root directory)
-db = sqlite3.connect('./backend/mhw.db')
+dbPath = './backend/mhw.db'
+db = sqlite3.connect(dbPath)
 
 # Create a cursor
 # (this is our connection to the database/will execute sql statements)
 curs = db.cursor()
 
-# Save an executed query to a variable (is a sqlite3 cursor object)
-all_mons = curs.execute("SELECT * FROM monsters")
+# DROP EXISTING DATA
+curs.execute("""
+DROP TABLE IF EXISTS monsters;
+""")
 
-# Iterate throught the data
-for mon in all_mons:
-    print(mon)
+curs.execute("""
+CREATE TABLE monsters (
+    monsterId integer PRIMARY KEY AUTOINCREMENT,
+    name varchar(20) NOT NULL UNIQUE,
+    type varchar(20),
+    size varchar(5)
+);
+""")
+
+# Check tables were made
+tableNames = curs.execute("SELECT name FROM sqlite_master;")
+print(tableNames.fetchall())
