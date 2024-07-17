12c538f6de5ec313101fc2965343ea70c97338c9
ansorokey <ansorokey@gmail.com>
2024-07-17 10:03:48 -0400

    wokring on tutorial, can create table

--- backend/sqlite3-tutorial.py
+++ backend/sqlite3-tutorial.py
@@ -1,2 +1,16 @@
 # Followed along from documentation
 # [https://docs.python.org/3/library/sqlite3.html#sqlite3-tutorial]
+
+# Import sqlite3 package
+import sqlite3
+
+# Choose the database to connect to
+# Will create database of the name if it cannot be found
+con = sqlite3.connect('tutorial.db')
+
+# Create a cursor
+# Connects to the database, is what we will use to execute statements
+cur = con.cursor()
+
+# Create a table using raw SQL
+cur.execute('CREATE TABLE movie(title, year, score)')
