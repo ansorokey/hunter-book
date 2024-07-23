c2e4a7ff150de7f7e7f385f5755b59ca9b638131
ansorokey <ansorokey@gmail.com>
2024-07-23 10:29:44 -0400

    added al lspecies to large monsters

--- backend/index.py
+++ backend/index.py
@@ -1,40 +1,15 @@
-import sqlite3
-from data import monsters
 
-# Establish the database we want to connect to
-# (Assume we're running this from root directory)
-dbPath = './backend/mhw.db'
-db = sqlite3.connect(dbPath)
+from data import largeMonsters
 
-# Create a cursor
-# (this is our connection to the database/will execute sql statements)
-curs = db.cursor()
+res = []
+for m in largeMonsters:
+    mon = {
+        'name': m[0],
+        'species': m[1],
+        'size': m[2]
+    }
+    res.append(mon)
 
-# DROP EXISTING DATA
-curs.execute("""
-DROP TABLE IF EXISTS monsters;
-""")
-
-# Create tables
-curs.execute("""
-CREATE TABLE monsters (
-    monsterId integer PRIMARY KEY AUTOINCREMENT,
-    name varchar(20) NOT NULL UNIQUE,
-    type varchar(20),
-    size varchar(5)
-);
-""")
-
-# Check tables were made
-tableNames = curs.execute("SELECT name FROM sqlite_master;")
-print(tableNames.fetchall())
-
-curs.executemany("""
-INSERT INTO monsters (name, size) VALUES (?, ?);
-""", monsters)
-
-checkMonsters = curs.execute("""
-SELECT * from MONSTERS limit 5;
-""");
-
-print(checkMonsters.fetchall())
+# Open the file - a flag to append (write to the end of the file)
+f = open('./data.py', 'a')
+f.write('largeMonsters = {0}'.format(res))
---
+++ backend/seed.py
@@ -0,0 +1,40 @@
+import sqlite3
+from data import smallMonsters, largeMonsters
+
+# Establish the database we want to connect to
+# (Assume we're running this from root directory)
+dbPath = './backend/mhw.db'
+db = sqlite3.connect(dbPath)
+
+# Create a cursor
+# (this is our connection to the database/will execute sql statements)
+curs = db.cursor()
+
+# DROP EXISTING DATA
+curs.execute("""
+DROP TABLE IF EXISTS monsters;
+""")
+
+# Create tables
+curs.execute("""
+CREATE TABLE monsters (
+    monsterId integer PRIMARY KEY AUTOINCREMENT,
+    name varchar(20) NOT NULL UNIQUE,
+    type varchar(20),
+    size varchar(5)
+);
+""")
+
+# Check tables were made
+tableNames = curs.execute("SELECT name FROM sqlite_master;")
+print(tableNames.fetchall())
+
+curs.executemany("""
+INSERT INTO monsters (name, size) VALUES (?, ?);
+""", monsters)
+
+checkMonsters = curs.execute("""
+SELECT * from MONSTERS limit 5;
+""");
+
+print(checkMonsters.fetchall())
